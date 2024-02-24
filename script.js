3c38191ef9703005884e5aca205766650962bb28
MDGuzma <mdguz99@gmail.com>
2024-02-24 15:01:44 -0500

    test

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const yesButton = document.querySelector("yesButton");
-
+const yesButton = document.getElementById("yesButton");
+const gif = document.getElementById("questionGif");
 const noButton = document.getElementById("noButton");
-
+const text = document.getElementById("questionText");
 
 function position() {
-    var x = Math.floor(Math.random() * 500) + 1;
-    var y = Math.floor(Math.random() * 200) + 1;
+    var x = Math.floor(Math.random() * 700) + 1;
+    var y = Math.floor(Math.random() * 100) + 1;
     noButton.style.left = x + "px";
     noButton.style.top = y + "px";
 
 }
 
-var words = ["Srs?,", "cmon", "You can do better"];
+// var words = ["Srs?,", "cmon", "You can do better", "Oops try again!", "NOT ALLOWED"];
+var words = ["srsly??", "Oops try again", "c'mon", "You can do better", "NOT ALLOWED", "Try again", "Keep going", "Give it another shot", "Don't give up", "Just one more time", "Keep trying", "Persevere", "Keep at it"]
+
 var currentIndex = 0;
 function randomWords() {
     currentIndex = (currentIndex + 1) % words.length;
@@ -25,3 +27,10 @@ noButton.addEventListener("mouseover", function () {
     randomWords();
 });
 
+yesButton.addEventListener("click", function () {
+    gif.src = "https://media1.tenor.com/m/UCbhfUXlmrgAAAAC/dudu-love-bubu-love.gif";
+    text.innerText = "YAHOOOOOO";
+    noButton.style.display = "none";
+    yesButton.style.display = "none";
+})
+let playSound = () => new Audio("/Users/mdguzman/Documents/markdeguzman/lovesong.mp3").play()
