a4fabd04836b583798df70869fa72e2469e10362
MDGuzma <mdguz99@gmail.com>
2024-02-26 00:55:08 -0500

    Huge changes. Completely redesigned using bootsrap CSS.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.onload = function () {
 
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector(".navbar").style.top = "0";
+        } else {
+            document.querySelector(".navbar").style.top = "-100px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 }
